c58b1eea238b89477728494db723b57acfb05c2d
Sam Billingham <sambillie.sb@gmail.com>
2014-11-17 12:59:00 +0000

    remove logic from view

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -8,8 +8,8 @@ class PlansController < ApplicationController
     end
 
     def show
-        @partners = ["architect", "resident", "community group", "politician"]
         @plan = Plan.find(params[:id])
+        @output = plan_output
     end
 
     def create
@@ -26,5 +26,33 @@ class PlansController < ApplicationController
     private
     def plan_params
         params.require(:plan).permit(:role, :funding, :people, :space, :partnerships, :scale, :opportunity, :timescale, :familiarity, :past_projects)
+    end
+
+    def plan_output
+        answers = []
+        if @plan.funding == false
+            answers.push("You need funding for your project")
+        end
+        if @plan.people == false
+            answers.push("You need volunteers for your project")
+        end
+        if @plan.space ==
+            answers.push("You need a space for your project")
+        end
+
+        if @plan.partnerships == "no partnerships"
+            answers.push("You should consider making partnerships with Architects, Residents, Community groups, and politicians to increase connetivity in the area")
+        else
+            @partners = ["architect", "resident", "community group", "politician"]
+            @partners.delete(@plan.partnerships)
+            message = "You should consider making partnerships with "
+            @partners.each do |partner|
+                message = message + partner + "s "
+            end
+            message = message + "as well as other " + @plan.partnerships + "s"
+            answers.push(message)
+        end
+
+        return answers
     end
 end
--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,12 +1,12 @@
 class Plan < ActiveRecord::Base
     validates :role, presence: true
-    validates :funding, presence: true
-    validates :people, presence: true
-    validates :space, presence: true
+    validates_inclusion_of :funding, :in => [true, false]
+    validates_inclusion_of :people, :in => [true, false]
+    validates_inclusion_of :space, :in => [true, false]
     validates :partnerships, presence: true
     validates :scale, presence: true
     validates :opportunity, presence: true
     validates :timescale, presence: true
     validates :familiarity, presence: true
-    validates :past_projects, presence: true
+    validates_inclusion_of :past_projects, :in => [true, false]
 end
