5b14d61b172d475451cfb82eb42c3f1075269c7f
Sam Billingham <sambillie.sb@gmail.com>
2014-11-17 16:28:06 +0000

    restructure output data, add 2 more outputs

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -18,7 +18,7 @@ class PlansController < ApplicationController
             flash[:success] = "Here is your suggested plan"
             redirect_to plan_path(@plan)
         else
-            flash[:error] = "Boo, Something went wrong"
+            flash[:danger] = "Boo, Something went wrong"
             render :new
         end
     end
@@ -30,18 +30,37 @@ class PlansController < ApplicationController
 
     def plan_output
         answers = []
+
+        case @plan.timescale
+        when 1
+            answers.push({:title => "Timescale", :info => ["smaller scale suggestion", "Strict budget, no additional funding", "Suggest longer timescale"]})
+        when 6
+            answers.push({:title => "Timescale", :info => ["May be able to get aditional funding depending on timescale", "Enough time to engage multiple parties"]})
+        when 12
+            answers.push({:title => "Timescale", :info => ["Advise Long projects must have planned series on engagement in orfer to maintain interest", "Good chance to gain outside funding", "make sure to create string connections and relationships during this time", "Good amount of time to conduct large scale project"]})
+        end
+
+        case @plan.familiarity
+        when "very"
+            answers.push({:title => "Familiarity", :info => ["Make use of connections that you are likely to have"]})
+        when "familiar"
+            answers.push({:title => "Familiarity", :info => ["Suggest improving/building upon connections"]})
+        when "unfamiliar"
+            answers.push({:title => "Familiarity", :info => ["Creating new connections will be important to ensuring success", "Suggest smaller projects leading to more projects", "long time span will be necessary"]})
+        end
+
         if @plan.funding == false
-            answers.push("You need funding for your project")
+            answers.push({:title => "Funding", :info => ["You need funding for your project"]})
         end
         if @plan.people == false
-            answers.push("You need volunteers for your project")
+            answers.push({:title => "People", :info => ["You need volunteers for your project"]})
         end
         if @plan.space ==
-            answers.push("You need a space for your project")
+            answers.push({:title => "Space", :info => ["You need a space for your project"]})
         end
 
         if @plan.partnerships == "no partnerships"
-            answers.push("You should consider making partnerships with Architects, Residents, Community groups, and politicians to increase connetivity in the area")
+            answers.push({:title => "Partnerships", :info => ["You should consider making partnerships with Architects, Residents, Community groups, and politicians to increase connetivity in the area"]})
         else
             @partners = ["architect", "resident", "community group", "politician"]
             @partners.delete(@plan.partnerships)
@@ -50,7 +69,7 @@ class PlansController < ApplicationController
                 message = message + partner + "s "
             end
             message = message + "as well as other " + @plan.partnerships + "s"
-            answers.push(message)
+            answers.push({:title => "Partnerships", :info => [message]})
         end
 
         return answers
