dd0f8b49bda4dd255a794f24d7505da7b57315ff
Sam Billingham <sambillie.sb@gmail.com>
2014-11-18 20:02:36 +0000

    add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   root 'plans#new'
 
+  get 'dashboard' => 'pages#dashboard'
+  get 'about' => 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
