7c15364c670893e9cc12d8311ca0650c3ce454e3
Sam Billingham <sambillie.sb@gmail.com>
2014-11-18 22:01:19 +0000

    fix syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method   = :postmark
-  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY']
+  config.action_mailer.postmark_settings = { :api_key => ENV['POSTMARK_API_KEY'] }
 end
