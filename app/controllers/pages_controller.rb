60026063141b63ba5748bd59cededb41a349221b
Sam Billingham <sambillie.sb@gmail.com>
2014-11-18 21:05:00 +0000

    auth for dashboard + dummy about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,10 @@ class PagesController < ApplicationController
     end
 
     def dashboard
+        unless admin_signed_in?
+            flash[:danger] = "You need to be an admin user to view this page"
+            redirect_to root_path
+        end
         @plans = Plan.all
     end
 end
