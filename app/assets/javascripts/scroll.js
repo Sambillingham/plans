8580ea1cab3d1a1804b1057a7be0acb3db2b02ee
Sam Billingham <sambillie.sb@gmail.com>
2014-12-21 15:26:42 +0000

    add scroll magic parallax to homepage

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,21 @@
+$(document).ready(function($) {
+
+    console.log("par");
+
+    var controller = new ScrollMagic({
+        globalSceneOptions: {
+            triggerHook: "onEnter",
+            duration: $(window).height()*2
+        }
+    });
+
+    new ScrollScene({triggerElement: ".js-parallax--author"})
+        .setTween(TweenMax.from(".js-parallax--author > .parallax__bg", 1, {top: "-80%", ease: Linear.easeNone}))
+        .addTo(controller);
+
+    new ScrollScene({triggerElement: ".js-parallax--toolkit"})
+        .setTween(TweenMax.from(".js-parallax--toolkit > .parallax__bg", 1, {top: "-130%", ease: Linear.easeNone}))
+        .addTo(controller);
+
+});
+
